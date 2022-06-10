d6b520f2c67434ac341a385a2060ff67b92f968d
equal-l2 <eng.equall2@gmail.com>
2022-06-10 16:55:33 +0900

    Increase the precision of the data in simplification
    closes #1

---
+++ geojson_to_json.py
@@ -0,0 +1,19 @@
+import json
+import sys
+
+# extract only coordinates from geojson
+
+filename = sys.argv[1]
+print(f"Opening {filename}")
+with open(filename, 'r') as f:
+    data = json.load(f)
+
+arr = []
+for ent in data["features"]:
+    coords = ent["geometry"]["coordinates"][0]
+    arr.append(coords)
+
+with open("out.json", "w") as f:
+    json.dump(arr, f)
+
+print("Wrote to out.json")
